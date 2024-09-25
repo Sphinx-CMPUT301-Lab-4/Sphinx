a5aecdde6a1c80574b4b443b6b502c7433baea85
I KHALID <ikhalid@ualberta.ca>
2024-09-24 21:56:35 -0500

    Added files

---
+++ app/src/main/java/com/example/sphinx/Shape.java
@@ -0,0 +1,16 @@
+package com.example.sphinx;
+
+class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+abstract class Shape{
+    int x;
+    int y;
+    public Shape(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
