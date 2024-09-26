90fb19906421ae3e2c6676203755e01050d2dffe
Adam Paunovic <adampaunovic4@gmail.com>
2024-09-25 23:04:45 -0600

    Added color to Shape

--- app/src/main/java/com/example/sphinx/Shape.java
+++ app/src/main/java/com/example/sphinx/Shape.java
@@ -6,9 +6,10 @@ class Main {
     }
 }
 
-abstract class Shape{
+abstract class Shape {
     int x;
     int y;
+    String color = "blue";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
