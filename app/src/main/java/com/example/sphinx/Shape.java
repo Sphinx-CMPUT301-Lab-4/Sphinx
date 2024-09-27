477431e28acd808ff66696393044727774b7ee18
Aniket Patel <ampatel@ualberta.ca>
2024-09-26 20:21:27 -0600

    Added color to Shape class

--- app/src/main/java/com/example/sphinx/Shape.java
+++ app/src/main/java/com/example/sphinx/Shape.java
@@ -9,8 +9,10 @@ class Main {
 abstract class Shape{
     int x;
     int y;
+    String colour;
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
+        this.colour = "Magenta";
     }
 }
