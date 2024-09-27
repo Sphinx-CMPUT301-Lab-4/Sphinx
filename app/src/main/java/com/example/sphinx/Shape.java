4c55af1b696b23edc002511158ce2e3d9f11514b
I KHALID <ikhalid@ualberta.ca>
2024-09-27 01:44:40 -0500

    before color attribute added

--- app/src/main/java/com/example/sphinx/Shape.java
+++ app/src/main/java/com/example/sphinx/Shape.java
@@ -9,7 +9,6 @@ class Main {
 abstract class Shape{
     int x;
     int y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
