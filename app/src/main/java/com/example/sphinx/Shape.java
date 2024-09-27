4708165b6bc710cd5cb87a6b8c146ddb261dc838
Rayyanshan <rshan1@ualberta.ca>
2024-09-26 19:58:51 -0600

    Merged Main into my branch.

--- app/src/main/java/com/example/sphinx/Shape.java
+++ app/src/main/java/com/example/sphinx/Shape.java
@@ -9,7 +9,7 @@ class Main {
 abstract class Shape{
     int x;
     int y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "red";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
